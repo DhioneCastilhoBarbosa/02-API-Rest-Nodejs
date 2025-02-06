1ffb19d112a70062cc62e19789164ab968a9c1a6
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-02-06 19:47:24 -0300

    fix: Env Port Render

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import { env } from './env'
 import { app } from './app'
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server is running on port: ${env.PORT}`)
-})
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
+  })
+  .then(() => {
+    console.log(`Server is running on port: ${env.PORT}`)
+  })
